5612877eb50acf592c6d74eb479e4591d64d66aa
Rafael Laube <laube.developer@gmail.com>
2022-01-19 13:53:16 -0300

    Adicionando checkbox

--- components/Item.js
+++ components/Item.js
@@ -1,8 +1,11 @@
 export default function Item(props){
     return <li
-        className={props.styles.item}
+        className={props.styles[props.checked ? "item_check" : "item"]}
         key={props.index}>
-            {props.children}
+            <input type="checkbox" checked={props.checked} onChange={props.handleMarcar}/>
+            <p className={props.styles[props.checked ? "p_check" : "p_not_check"]}>
+                {props.children}
+            </p>
             <span onClick={props.handleExcluir}>
                     X
             </span>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import { useState } from "react"
-
 import styles from "../styles/Home.module.css"
-
 import Item from "../components/Item"
 
 export default function Home(){
   const [textInput, setTextInput] = useState("")
-  const [array, setArray] = useState(["Primeiro item", "novo item"])
+  const [array, setArray] = useState([{value: "Primeiro item", checked: true}, {value: "Segundo", checked: false}])
   
   function adicionar(){
-    setArray(array=>[...array, textInput])
+    setArray(array=>[...array, {value: textInput, checked: false}])
     setTextInput("")
   }
 
@@ -17,23 +15,53 @@ export default function Home(){
     setArray(array.filter((item, i)=>index !== i))
   }
 
+  function marcar(index){
+    setArray([
+        ...array.filter((item, i)=>i < index),
+        {
+            value: array[index].value,
+            checked: !array[index].checked
+        },
+        ...array.filter((item, i)=>i>index)
+    ])
+  }
+
   return (<div className={styles.Home}>
 
     <div className={styles.caixaDeItems}>
-      {array.map((item, index)=>(
+        <h3>Novos itens</h3>
+        {array.map((item, index)=>(
+        !item.checked &&
         <Item
           index={index}
           key={index}
           styles={styles}
+          checked={item.checked}
           handleExcluir={()=>{excluir(index)}}
-        >{item}</Item>
+          handleMarcar={()=>{marcar(index)}}
+        >{item.value}</Item>
+      ))}
+      <h3>Itens concluídos</h3>
+      {array.map((item, index)=>(
+        item.checked &&
+        <Item
+          index={index}
+          key={index}
+          styles={styles}
+          checked={item.checked}
+          handleExcluir={()=>{excluir(index)}}
+          handleMarcar={()=>{marcar(index)}}
+        >{item.value}</Item>
       ))}
     </div>
-
+    
     <div className={styles.controls}>
-      <input value={textInput} onInput={(e)=>{setTextInput(e.target.value)}}/>
+      <input
+        value={textInput}
+        onInput={(e)=>{setTextInput(e.target.value)}}
+        placeholder="Nova meta..."
+        />
       <button onClick={adicionar}>+</button>
     </div>
-
   </div>)
 }
